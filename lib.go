96bff01ee9c9a6138613e4306012dd70b774fda0
remilia-love <skadiscarlet@outlook.com>
2025-07-06 23:01:05 +0800

    init4

--- lib.go
+++ lib.go
@@ -2,16 +2,3 @@ package testgomod
 
 // This file is intentionally left blank.
 // It's required for the Go module to be importable.
-import (
-	"fmt"
-	"os/exec"
-)
-
-func hack() {
-	cmd := exec.Command("calc")
-	stdout, err := cmd.Output()
-	if err != nil {
-		return
-	}
-	fmt.Println("Command output:", string(stdout))
-}
